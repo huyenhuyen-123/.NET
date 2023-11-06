fc4c9b9dbd07e2c4336e8504bff9e6301417a377
huyenhuyen-123 <128460372+huyenhuyen-123@users.noreply.github.com>
2023-11-06 19:13:57 +0700

    up file

---
+++ NhapHangView/NhapHangView.cs
@@ -0,0 +1,40 @@
+﻿using NhapHang.Utils;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace NhapHang
+{
+    public partial class NhapHangView : Form
+    {
+
+        public NhapHangView()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            using (SqlConnection conn = DatabaseHelper.GetConnection())
+            {
+                conn.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM HANGHOA", conn);
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+
+                DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
+                comboBoxColumn.DataSource = dt;
+                comboBoxColumn.DataPropertyName = "ID";
+                comboBoxColumn.HeaderText = "MaHang";
+                comboBoxColumn.Name = "Column1";
+
+                dataGridView1.Columns.Insert(1, comboBoxColumn);
+            }
+        }
+    }
+}
---
+++ Utils/DatabaseHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhapHang.Utils
+{
+    internal class DatabaseHelper
+    {
+
+    public static SqlConnection GetConnection()
+        {
+            return new SqlConnection("Server=LAPTOP-QGBKFEJQ\\SQLEXPRESS;Database=KHOHANG;User Name=sa;Password=123456");
+
+        }
+    }
+}
+
